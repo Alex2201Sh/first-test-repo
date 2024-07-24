6b396fb8a07e6239cad0f41139f96504dc063d12
Alexei Shumilov <shumilov218@gmail.com>
2024-07-24 19:23:49 +0300

    second commit

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
-public class Test{
+public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello Git");
+        }
     }
 }
